f3a93f6d514037661539dcbe624a194ae44d8bad
Brian Dodge <briandodge85@gmail.com>
2019-04-23 10:58:39 +0100

    Removed annoying alert

--- resources/defaultscript.js
+++ resources/defaultscript.js
@@ -1,10 +1,6 @@
 $(function() {
-
-    alert("Loading content");
     
-    $("#headerpane").load("./pages/header.html", function() {
-        $("#headerpane").fadeIn(800);
-    });
+    $("#headerpane").load("./pages/header.html");
 
     $("#contentpane").load("./pages/content.html");
 
